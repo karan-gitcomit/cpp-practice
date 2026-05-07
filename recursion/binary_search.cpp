f067fae2edcabf10a9c27638c6675784de66a39e
karan <saikaranshravankumarchutkay@gmail.com>
2026-05-07 22:13:22 +0530

    Update binary_search

--- recursion/binary_search.cpp
+++ recursion/binary_search.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <algorithm>
 
 int binarySearch(int arr[], int low, int high, int target){
-    int mid = (low + high) / 2;
-
     if(low > high) return -1;
+    int mid = (low + high) / 2;
     if(arr[mid] == target) return mid;
-    
-    if(low > mid)
+    if(target < arr[mid]) return binarySearch(arr, low, mid - 1, target);
+    else{
+        return binarySearch(arr, mid + 1, high, target);
+    }
+}
+
+int main(){
+    int arr[] = {11, 3, 6, 9, 13, 9, 8};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    std::sort(arr, arr + size);
+    int result = binarySearch(arr, 0, size - 1, 3);
+    std::cout << "The index of the target element is " << result << std::endl;
 }
